ce24746ce0f7631eb24730dc7a5a86aa7fdaeb2a
AllenliIE <allen.li.service@gmail.com>
2022-07-03 23:29:33 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose') //載入 mongoose
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
+const methodOverride = require('method-override')
 const Todo = require('./models/todo')
 const todo = require('./models/todo')
 
@@ -26,6 +27,7 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   //拿到全部的 Todo 資料
@@ -69,7 +71,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
 
@@ -79,11 +81,11 @@ app.post('/todos/:id/edit', (req, res) => {
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos${id}`))
     .catch(error => console.log)
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
