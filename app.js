e48ef025d58577bb1f8248ad5ea001d6247d2a40
AllenliIE <allen.li.service@gmail.com>
2022-07-04 09:11:54 +0800

    refactor: modigy mongoose config

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') //載入 mongoose
-
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到 mongoDB
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
