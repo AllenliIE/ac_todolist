ecd8c2b945aaa24a99ae773536eb858823bbe48c
AllenliIE <allen.li.service@gmail.com>
2022-08-26 00:08:01 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(session({
 
 usePassport(app)
 
+//middleware
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
